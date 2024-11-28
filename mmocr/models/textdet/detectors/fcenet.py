db0b019df7936090c907799e11c1f5b3dc633b26
Chris Olivier <cjolivier01@apache.org>
2024-11-27 18:04:28 -0800

    ..

--- mmocr/models/textdet/detectors/fcenet.py
+++ mmocr/models/textdet/detectors/fcenet.py
@@ -1,5 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import time
+
 from mmocr.registry import MODELS
+
 from .single_stage_text_detector import SingleStageTextDetector
 
 
@@ -11,3 +14,17 @@ class FCENet(SingleStageTextDetector):
 
     [https://arxiv.org/abs/2104.10442]
     """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._counter: int = 0
+        self._acc_time: float = 0.0
+
+    def __call__(self, *args, **kwargs):
+        start = time.time()
+        results = super().__call__(*args, **kwargs)
+        self._acc_time += time.time() - start
+        self._counter += 1
+        if self._counter % 50:
+            print(f"FCENet time: {self._counter / self._acc_time} fps each")
+            self._counter = 0
+        return results
